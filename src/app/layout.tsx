17fc3c4f4df35b5bf88ac1933852d46093dd92d8
jkh675 <rluo2018@gmail.com>
2023-05-13 12:34:46 +0800

    [feat](add sidebar compomnet):

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import SideBar from './sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <SideBar>{children}</SideBar>
         </html>
     )
 }

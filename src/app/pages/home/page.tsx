2fba7d8d51df02f44292d4f681294e54971d929b
jkh675 <rluo2018@gmail.com>
2023-06-27 18:17:22 +0800

    edge compatible

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,3 +1,10 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'edge'
+export const preferredRegion = 'auto'
+
 import styles from './page.module.css'
 
 export default function HomePage() {
--- src/app/pages/shooter/statis/[id]/page.tsx
+++ src/app/pages/shooter/statis/[id]/page.tsx
@@ -1,3 +1,10 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = false
+export const fetchCache = 'auto'
+export const runtime = 'edge'
+export const preferredRegion = 'auto'
+
 export default function ShooterStatisPage(props: { params: { id: any } }) {
     return (
         <div>
